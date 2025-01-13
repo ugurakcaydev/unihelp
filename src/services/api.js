5eb3c10e6e14a2af47539f39e83b32a55b858f35
Uğur Akçay <akcayugur01@gmail.com>
2025-01-13 20:31:00 +0300

    aaa

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = import.meta.env.VITE_API_URL;
+export const baseURL = "https://api.berkormanli.dev/unihelp/api";
 
 const api = axios.create({
   baseURL,
